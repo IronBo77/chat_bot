0e70eb39313cbf176cba5dac96987c688a4b7153
IronBo77 <37585195+IronBo77@users.noreply.github.com>
2020-01-04 19:20:47 +0300

    Добавлены: обработчик новых членов группы, ежедневная отправка сообщений

--- stec_st_bot.py
+++ stec_st_bot.py
@@ -4,9 +4,13 @@ from telegram.ext import Filters
 from telegram.ext import Updater
 from telegram.ext import CommandHandler
 import logging
+import datetime
 import os
 
 
+'''
+#При использовании прокси
+#PROXY = {'proxy_url':'socks5://179.43.157.119:1080'}
 REQUEST_KWARGS={
     'proxy_url':'socks5://179.43.157.119:1080',
     # Optional, if you need authentication:
@@ -17,18 +21,32 @@ REQUEST_KWARGS={
         # 'password': 'password'
     }
     }
+'''
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO,
                     filename= 'bot.log'
                     )
 
+#Отвечает за отправку ежедневных сообщений
+def sender(context):
+    context.bot.send_message(-1001171884119,'SEND JOB')
+
+#Обработка команды старт
 def greet_user(update,context):
     text = 'Vizvan start'
     update.message.reply_text(text)
+    context.bot.send_message(-1001171884119,'SEND')
     logging.info(text)
 
+#Отвечает за добавление новых участников
+def new_member(update,context):
+    context.bot.send_message(-1001171884119, 'New Chat Member')
+
+
+#Обработка сообщений
 def talk_to_me(update,context):
+
     if update.message.text=='тест':
         user_text = f'Привет {update.message.from_user.first_name} ({update.message.from_user.username})! ' \
                 f'Ты написал "{update.message.text}".'
@@ -40,15 +58,25 @@ def talk_to_me(update,context):
         update.message.reply_text(user_text)
 
 
-
 def main():
     TOKEN = os.environ.get('MY_TOKEN')
     mybot = Updater(token=str(TOKEN),use_context=True)
-    dp = mybot.dispatcher
 
+    dp = mybot.dispatcher
+    # Добавляем задание в очередь
+    job = mybot.job_queue
     print('Бот стартовал')
-    dp.add_handler(CommandHandler("start", greet_user))
-    dp.add_handler(MessageHandler(Filters.text,talk_to_me))
+    # Время дня в которое будет выполнятся ежедневное задание
+    time = datetime.time(12, 0, 0)
+    # Добавляем задание
+    job.run_daily(sender, time)
+    # Обработчик команд
+    dp.add_handler(CommandHandler("start", sender))
+    # Обработчик сообщений
+    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+    # Обработчик добавления новых участников
+    dp.add_handler(MessageHandler(Filters.status_update.new_chat_members, new_member))
+
     mybot.start_polling()
     mybot.idle()
 
